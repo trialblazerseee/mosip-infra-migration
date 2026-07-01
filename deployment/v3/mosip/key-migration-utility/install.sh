d2bb2758d0f1086f2eeb5928190ea95385f5b7e5
trialblazerseee <84778104+trialblazerseee@users.noreply.github.com>
2026-07-01 19:02:30 +0530

    KeyManager Modification for MountPath

--- deployment/v3/mosip/key-migration-utility/install.sh
+++ deployment/v3/mosip/key-migration-utility/install.sh
@@ -39,7 +39,7 @@ function installing_key_migration_utility() {
   --set softHsmCM=softhsm-$module-share \
   --set springConfigNameEnv=$config_prop \
   --set extraEnvVars[0].name=SPRING_MAIN_ALLOW-CIRCULAR-REFERENCES \
-  --set extraEnvVars[0].value=true \
+  --set-string extraEnvVars[0].value=true \
   --wait --wait-for-jobs \
   --version $CHART_VERSION \
   -f keymgr.yaml
