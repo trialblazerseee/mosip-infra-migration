78162fab167888a6e2f178ee99f2eda85bc8d16c
trialblazerseee <84778104+trialblazerseee@users.noreply.github.com>
2026-07-03 10:43:11 +0530

    KeyManager Modification for MountPath

--- deployment/v3/mosip/keymanager/copy_cm.sh
+++ deployment/v3/mosip/keymanager/copy_cm.sh
@@ -4,12 +4,12 @@
 
 function copying_cm() {
   COPY_UTIL=../../utils/copy_cm_func.sh
-  DST_NS=keymanager
+  DST_NS=ida
 
   $COPY_UTIL configmap global default $DST_NS
   $COPY_UTIL configmap artifactory-share artifactory $DST_NS
   $COPY_UTIL configmap config-server-share config-server $DST_NS
-  $COPY_UTIL configmap softhsm-kernel-share softhsm $DST_NS
+  $COPY_UTIL configmap softhsm-ida-share softhsm $DST_NS
   return 0
 }
 
