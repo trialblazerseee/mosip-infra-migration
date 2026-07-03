c8996f66d7e12802cfac34b2b81cd3bde7171220
trialblazerseee <84778104+trialblazerseee@users.noreply.github.com>
2026-07-03 10:15:33 +0530

    KeyManager Modification for MountPath

--- deployment/v3/mosip/keymanager/delete.sh
+++ deployment/v3/mosip/keymanager/delete.sh
@@ -7,7 +7,7 @@ if [ $# -ge 1 ] ; then
 fi
 
 function deleting_keymanager() {
-  NS=keymanager
+  NS=ida
   while true; do
       read -p "Are you sure you want to delete keymanager helm chart?(Y/n) " yn
       if [ $yn = "Y" ]
